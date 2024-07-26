56a73d8cab7dda64de85f5bffb6f1f74f6c9c5d7
i-hidaka <i.hidaka08@gmail.com>
2024-07-26 09:12:58 +0900

    ✨ 投稿画面のモック作成

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
       <div>
         <nav className="text-sm font-medium">
           <Link
-            href="/article/new"
+            href="/articles/new"
             className="bg-orange-300 px-3 py-3 rounded-md"
           >
             記事を書く
