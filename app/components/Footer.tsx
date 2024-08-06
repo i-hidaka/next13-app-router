2e4f576c0a2ab70223050c9a71d275409df355f9
i-hidaka <i.hidaka08@gmail.com>
2024-08-06 09:38:29 +0900

    🩹 フッターの修正

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className="py-2 px-4">@2024 i-hidaka</footer>;
+  return <footer className="py-2 px-4 border-t">@2024 i-hidaka</footer>;
 };
 
 export default Footer;
